47b9573526f996392b81e66607a08e3dd11d08ee
lukang-local <lukang-local@example.com>
2019-08-01 22:28:34 +0800

    fix

--- HashTable/error.c
+++ HashTable/error.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#define NULLVALUE -3026
+#define NULLVALUE 0//这里我使用0去填充
 #define VISITED 1
 #define NOTVISITED 0
 #define SIZE 16
@@ -13,6 +13,7 @@ typedef enum my_bool{
   my_false=0,my_true
 }my_bool;
 my_bool myHashSetSearch(MyHashSet *obj,int key,int *addr);
+void printf_hash(MyHashSet *obj);
 MyHashSet* myHashSetCreate() {
     MyHashSet *H=(MyHashSet *)malloc(sizeof(MyHashSet));
     H->hash=(int *)malloc(sizeof(int)*SIZE);
@@ -38,6 +39,7 @@ int hash(int key){
  */
 void myHashSetAdd(MyHashSet* obj, int key) {
     int addr=hash(key);
+    printf("%d %d %d\n",key,addr,myHashSetSearch(obj,key,&addr));
     if(!myHashSetSearch(obj,key,&addr)){
       //如果这个坑被别人占了，找下一个坑
       while(obj->visited[addr]==VISITED){
@@ -46,6 +48,9 @@ void myHashSetAdd(MyHashSet* obj, int key) {
       obj->hash[addr]=key;
       obj->visited[addr]=VISITED; 
     } 
+    printf("*************\n");
+    printf_hash(obj);
+
 }
 /**
  * search
@@ -87,14 +92,20 @@ void printf_hash(MyHashSet *obj){
     printf("%4d",obj->hash[i]);
   }
   printf("\n");
+  for(int i=0;i<SIZE;i++){
+    printf("%4d",obj->visited[i]);
+  }
+  printf("\n");
 }
 int main(){
   int length=16;
-  int keys[]={-20,-2,-11,0,12,67,56,16,25,37,22,29,15,47,48,34};
+  int keys[]={-20,-2,-11,1,12,67,56,16,25,37,22,29,15,47,48,34};
   MyHashSet *H=myHashSetCreate();
+  printf_hash(H);
   for(int i=0;i<length;i++){
     myHashSetAdd(H,keys[i]);
   }
   printf_hash(H);
   return 0;
+  
 }

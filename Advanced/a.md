b7df2bf58f8d065f8b508bd110d562d7e3a6e5ee
lukang-local <lukang-local@example.com>
2019-08-04 22:19:06 +0800

    fix 0-1背包

--- Advanced/a.md
+++ Advanced/a.md
@@ -62,3 +62,25 @@ int main(int argc,char *argv[]){
   return 0;
 }
 ```
+### 二维动态数组的初始化与遍历
+```c
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define ROW 2
+#define COL 3
+int main(){
+  int **b=(int **)malloc(ROW*sizeof(int *));
+  for(int i=0;i<ROW;i++){
+    b[i]=(int *)malloc(COL*sizeof(int));
+    memset(b[i],0,COL*sizeof(int));
+  }
+  for(int i=0;i<ROW;i++){
+    for(int j=0;j<COL;j++){
+      printf("%4d",*(b[i]+j));
+    }
+    printf("\n");
+  }
+  return 0;
+}
+```
--- knapsack/ZeroOnePackage.c
+++ knapsack/ZeroOnePackage.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 #include <string.h>
 #define N 5  //商品个数
@@ -34,5 +35,71 @@ int main(){
   //创建价值矩阵
   printf("****************************************************\n");
   printf_arr(maxValue,N+1,C+1);
+  return 0;
+}
+*/
+/**
+ * 0-1背包问题
+ * 输入，商品的数目N,背包的容量或者你的总钱币M
+ * 每一行输入代表每一件商品的体积或者单价p,需要求的每一件商品的标准I,每一件商品有几个C(0-1背包里面默认都只有1个)
+ * 输入N行截止
+ * 输出需要求的商品的最大标准
+ * 5 10
+ * 4 6
+ * 5 4
+ * 6 5
+ * 2 3
+ * 2 6
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void printf_arr(int *arr,int len){
+  for(int i=0;i<len;i++){
+    printf("%4d",*(arr+i));
+  }
+  printf("\n");
+}
+void printf_2arr(int **arr,int N,int M){
+   for(int i=0;i<N+1;i++){
+    for(int j=0;j<M+1;j++){
+      printf("%4d",*(arr[i]+j));
+    }
+    printf("\n");
+  }
+}
+int **createWantArr(int N,int M,int *price,int *want){
+  int **arr=(int **)malloc((N+1)*sizeof(int *));
+  for(int i=0;i<N+1;i++){
+    arr[i]=(int *)malloc((M+1)*sizeof(int));
+    memset(arr[i],0,(M+1)*sizeof(int));
+  }
+  return arr;
+}
+int main(){
+  int N,M;
+  while(1){
+    scanf("%d%d",&N,&M);
+    int *price=(int *)malloc((N+1)*sizeof(int));
+    int *want=(int *)malloc((N+1)*sizeof(int));
+    memset(price,0,(N+1)*sizeof(int));
+    memset(want,0,(N+1)*sizeof(int));
+    for(int i=1;i<=N;i++){
+      int Price,Want;
+      scanf("%d%d",&Price,&Want);
+      *(price+i)=Price;
+      *(want+i)=Want;
+    }
+    /**
+     * price={0,4,5,6,2,2};
+     * want={0,6,4,5,3,6};
+     */
+    printf_arr(price,N+1);
+    printf_arr(want,N+1);
+    //初始化价值矩阵
+    int **arr=createWantArr(N,M,price,want);
+    printf_2arr(arr,N,M);
+  }
+
   return 0;
 }

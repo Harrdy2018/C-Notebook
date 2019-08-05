956d1c9b74ab822ae520e6d2f66c971ae8989b4a
Harrdy <1035525823@qq.com>
2019-08-05 10:01:47 +0800

    Update ZeroOnePackage.c

--- knapsack/ZeroOnePackage.c
+++ knapsack/ZeroOnePackage.c
@@ -1,43 +1,3 @@
-/*
-#include <stdio.h>
-#include <string.h>
-#define N 5  //商品个数
-#define C 10 //背包容量
-void printf_arr(int arr[][C+1],int row,int col){
-  for(int i=0;i<row;i++){
-    for(int j=0;j<col;j++){
-      printf("%4d",arr[i][j]);
-    }
-    printf("\n");
-  }
-}
-void createArr(int arr[][C+1],int weight[],int value[]){
-  for(int col=1;col<=C;col++){
-    for(int row=1;row<=N;row++){
-      if(weight[row]>col){
-        arr[row][col]=arr[row-1][col];
-      }else{
-        int a=arr[row-1][col];
-        int b=value[row]+arr[row-1][col-weight[row]];
-        arr[row][col]=(a>b ? a :b);
-      }
-    }
-  }
-}
-int main(){
-  int weight[]={0,4,5,6,2,2};
-  int value[]={0,6,4,5,3,6};
-  int maxValue[N+1][C+1];
-  memset(maxValue,0,sizeof(maxValue));
-  //初始化价值矩阵
-  printf_arr(maxValue,N+1,C+1);
-  createArr(maxValue,weight,value);
-  //创建价值矩阵
-  printf("****************************************************\n");
-  printf_arr(maxValue,N+1,C+1);
-  return 0;
-}
-*/
 /**
  * 0-1背包问题
  * 输入，商品的数目N,背包的容量或者你的总钱币M
@@ -74,7 +34,26 @@ int **createWantArr(int N,int M,int *price,int *want){
     arr[i]=(int *)malloc((M+1)*sizeof(int));
     memset(arr[i],0,(M+1)*sizeof(int));
   }
+  for(int row=1;row<=N;row++){
+    for(int col=1;col<=M;col++){
+      if(col<*(price+row)){
+        *(arr[row]+col)=*(arr[row-1]+col);
+      }else{
+        int a=*(arr[row-1]+col);
+        int b=*(price+row)+*(arr[row-1]+col-*(price+row));
+        *(arr[row]+col)=(a>b ? a :b);
+      }
+    }
+  }
   return arr;
+}
+void myArrFree(int **arr,int N,int M){
+  for(int i=0;i<=N;i++){
+    free(arr[i]);
+    arr[i]=NULL;
+  }
+  free(arr);
+  arr=NULL;
 }
 int main(){
   int N,M;
@@ -99,7 +78,8 @@ int main(){
     //初始化价值矩阵
     int **arr=createWantArr(N,M,price,want);
     printf_2arr(arr,N,M);
+    printf("%d\n",*(arr[N]+M));
+    myArrFree(arr,N,M);
   }
-
   return 0;
 }

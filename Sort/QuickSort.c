ecc631bf5d09485670f33f1f82c166ff0726406b
Harrdy <1035525823@qq.com>
2020-09-11 16:45:12 +0800

    Update QuickSort.c
    标准的 快排

--- Sort/QuickSort.c
+++ Sort/QuickSort.c
@@ -45,3 +45,52 @@ int main(){
   PrintfArr(arr,9);
   return 0;
 }
+
+
+/*
+#include <stdio.h>
+void swap(int arr[], int i, int j)
+{
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+    return;
+}
+void quickSort(int arr[], int start, int end)
+{
+    if (start >= end) {
+        // 当base被换到的位置刚好处于边界的时候
+        printf("start %d end %d\n", start, end);
+        return;
+    }
+    int base = arr[start];
+    int left = start;
+    int right = end;
+    while (left < right) {
+        // right 先走
+        while (arr[right] >= base && left < right) {
+            right--;
+        }
+        // left 先走
+        while (arr[left] <= base && left < right) {
+            left++;
+        }
+        // 满足条件交换(最好情况下，第一次就碰头了)
+        swap(arr, left, right);
+    }
+    // left 和 right 碰头
+    swap(arr, start, left);
+
+    quickSort(arr, start, left - 1);
+    quickSort(arr, left + 1, end);
+}
+int main(int argc, char *argv[])
+{
+    int arr[10] = {6, 1, 2, 7, 9, 3, 4, 5, 10, 8};
+    quickSort(arr, 0, 9);
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", arr[i]);
+    }
+    return 0;
+}
+*/
